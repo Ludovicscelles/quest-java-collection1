86c028998d0fe1c1efd0de322f3ef02a293b9047
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-14 19:32:07 +0100

    Challenge/Arraylist(java): create ArrayList heroes and test methods

--- Thanos.java
+++ Thanos.java
@@ -4,7 +4,8 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
+        // TODO 1 : Create an empty heroes listı
+        ArrayList<Hero> heroes = new ArrayList<>();
 
         // TODO 2 : Add those heroes to the list
         // name: Black Widow, age: 34
@@ -16,12 +17,58 @@ public class Thanos {
         // name: Hulk, age: 49
         // name: Doctor Strange, age: 42
 
+        Hero blackWidow = new Hero("Blac Widow", 34);
+        Hero captainAmerica = new Hero("Captain America", 100);
+        Hero vision = new Hero("Vision", 3);
+        Hero ironMan = new Hero("Iron Man", 48);
+        Hero scarletWitch = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero("Thor", 1500);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero doctorStrange = new Hero("Doctor Strange", 42);
+
+        heroes.add(blackWidow);
+        heroes.add(captainAmerica);
+        heroes.add(vision);
+        heroes.add(ironMan);
+        heroes.add(scarletWitch);
+        heroes.add(thor);
+        heroes.add(hulk);
+        heroes.add(doctorStrange);
+
+        System.out.println();
+        System.out.println("Voici la liste complète des héros : ");
+        for (Hero hero : heroes) {
+            System.out.println("- " + hero.getName() + " : " + hero.getAge() + " ans");
+        }
+
         // TODO 3 : It's Thor birthday, now he's 1501
 
+        Hero thor1501 = new Hero("Thor", 1501);
+        heroes.set(5, thor1501);
+        Hero thorUpdated = heroes.get(5);
+
+        System.out.println();
+        System.out.println("Voici Thor avec son age mis à jour : ");
+        System.out.println("- " + thorUpdated.getName() + " : " + thorUpdated.getAge() + " ans");
+
         // TODO 4 : Shuffle the heroes list
+        Collections.shuffle(heroes);
+
+        System.out.println();
+        System.out.println("Voici la liste des héros mélangée : ");
+        for (Hero hero : heroes) {
+            System.out.println("- " + hero.getName() + " : " + hero.getAge() + " ans");
+        }
 
         // TODO 5 : Keep only the half of the list
+        List<Hero> subListHeroes = heroes.subList(0, 4);
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+
+        System.out.println("Voici la liste des héros mélangée et réduite : ");
+        for (Hero hero : subListHeroes) {
+            System.out.println("- " + hero.getName() + " : " + hero.getAge() + " ans");
+        }
+
     }
 }
